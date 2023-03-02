ae446d7597b623c9ec456494151675576ae92225
MarkBrianK <kariukimarkbrian@gmail.com>
2023-03-02 16:29:11 +0300

    changing the caurosel image height

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,19 +14,27 @@ function About() {
       <Container className="py-5">
         <Row>
           <Col md={{ span: 8, offset: 2 }} className="text-white text-center">
-            <h1 className="display-3 mb-4" >About Us</h1>
-            <p>
-              Label is a fashion e-commerce website that offers a curated selection of clothing, accessories, and footwear for men and women. Our products are carefully selected from top brands and designers, ensuring that our customers have access to the latest trends and styles.
-            </p>
-            <p>
-              We believe that fashion should be accessible to everyone, which is why we offer a wide range of sizes and styles to suit every body type and personal preference. Whether you're looking for casual wear or formal attire, Label has something for everyone.
-            </p>
-            <p>
-              Our website is designed to provide a seamless shopping experience for our customers. You can easily browse our products, filter by size or color, and add items to your cart with just a few clicks. We also offer free shipping and returns to make your shopping experience even more convenient.
-            </p>
-            <p>
-              At Label, we are committed to providing excellent customer service. If you have any questions or concerns, our friendly and knowledgeable team is always here to help. We strive to make every customer feel valued and satisfied with their purchase.
-            </p>
+            <h1 className="display-3 mb-4">About Us</h1>
+
+            <Row>
+              <Col xs={12} md={6}>
+                <p>
+                  Label is a fashion e-commerce website that offers a curated selection of clothing, accessories, and footwear for men and women. Our products are carefully selected from top brands and designers, ensuring that our customers have access to the latest trends and styles.
+                </p>
+                <p>
+                  We believe that fashion should be accessible to everyone, which is why we offer a wide range of sizes and styles to suit every body type and personal preference. Whether you're looking for casual wear or formal attire, Label has something for everyone.
+                </p>
+              </Col>
+              <Col xs={12} md={6}>
+                <p>
+                  Our website is designed to provide a seamless shopping experience for our customers. You can easily browse our products, filter by size or color, and add items to your cart with just a few clicks. We also offer free shipping and returns to make your shopping experience even more convenient.
+                </p>
+                <p>
+                  At Label, we are committed to providing excellent customer service. If you have any questions or concerns, our friendly and knowledgeable team is always here to help. We strive to make every customer feel valued and satisfied with their purchase.
+                </p>
+              </Col>
+            </Row>
+
           </Col>
         </Row>
       </Container>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -25,17 +25,17 @@ function Home() {
           <img
             src="http://cdn.shopify.com/s/files/1/0928/3306/products/men-dress-shirts-100-cotton-plaid-shirt-men-plus-shirt-size-shirt-kenya.jpg?v=1586142650"
             alt="First slide"
-            style={{ width: '33.33%', height: '50vh', marginRight:"15px" }}
+            style={{ width: '33.33%', maxHeight: '50vh', marginRight:"15px" }}
           />
           <img
             src="https://media.istockphoto.com/id/864505242/photo/mens-clothing-and-personal-accessories.jpg?s=612x612&w=0&k=20&c=TaJuW3UY9IZMijRrj1IdJRwd6iWzXBlrZyQd1uyBzEY="
             alt="Second slide"
-            style={{ width: '33.33%', height: '50vh', marginRight:"15px" }}
+            style={{ width: '33.33%', maxHeight: '50vh', marginRight:"15px" }}
           />
           <img
             src="https://i.pinimg.com/originals/84/f6/93/84f693c1afbb4a68078407465bb8c6f3.jpg"
             alt="Third slide"
-            style={{ width: '33.33%', height: '50vh' }}
+            style={{ width: '33.33%', maxHeight: '50vh' }}
           />
         </div>
       </Carousel.Item>
@@ -44,12 +44,12 @@ function Home() {
       <img
         src="https://images.pexels.com/photos/19090/pexels-photo.jpg?cs=srgb&dl=pexels-web-donut-19090.jpg&fm=jpg"
         alt="First slide"
-        style={{ width: '33.33%', height: '50vh', marginRight:"15px" }}
+        style={{ width: '33.33%', maxHeight: '50vh', marginRight:"15px" }}
       />
       <img
         src="https://images.pexels.com/photos/267301/pexels-photo-267301.jpeg?cs=srgb&dl=pexels-pixabay-267301.jpg&fm=jpg"
         alt="Second slide"
-        style={{ width: '33.33%', height: '50vh', marginRight:"15px" }}
+        style={{ width: '33.33%', maxHeight: '50vh', marginRight:"15px" }}
       />
       <img
         src="https://image.kilimall.com/kenya/shop/store/goods/5968/2021/06/5968_06767302645445915_1280.jpg.webp"
@@ -62,17 +62,17 @@ function Home() {
       <img
         src="https://media.istockphoto.com/id/1208148708/photo/polka-dot-summer-brown-dress-suede-wedge-sandals-eco-straw-tote-bag-cosmetics-on-a-light.jpg?s=612x612&w=0&k=20&c=9Y135GYKHLlPotGIfynBbMPhXNbYeuDuFzreL_nfDE8="
         alt="First slide"
-        style={{ width: '33.33%', height: '50vh', marginRight:"15px" }}
+        style={{ width: '33.33%', maxHeight: '50vh', marginRight:"15px" }}
       />
       <img
         src="https://www.stitchfix.com/women/blog/wp-content/uploads/20-07-13_-SET_1_W_OLD-_v1_1x1-scaled.jpeg"
         alt="Second slide"
-        style={{ width: '33.33%', height: '50vh', marginRight:"15px" }}
+        style={{ width: '33.33%', maxHeight: '50vh', marginRight:"15px" }}
       />
       <img
         src="https://www.stitchfix.com/women/blog/wp-content/uploads/sv_231047_flat-v164-2-scaled.jpeg"
         alt="Third slide"
-        style={{ width: '33.33%', height: '50vh' }}
+        style={{ width: '33.33%', maxHeight: '50vh' }}
       />
     </div>
       </Carousel.Item>
@@ -88,7 +88,7 @@ function Home() {
     <a href="#" > <img src={puma} /></a>
     <a href="#" > <img src={reebok} /></a>
     <a href="#" > <img src={versace} /></a>
-   
+
     </div>
 
     </div>
