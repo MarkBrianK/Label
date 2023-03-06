2201319bc14ae586b224748a1ca49a519291b1e7
MarkBrianK <kariukimarkbrian@gmail.com>
2023-03-06 15:35:11 +0300

    changing the navigation

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -9,7 +9,7 @@ function Navigation() {
     <Navbar expand="md" className='navigation' collapseOnSelect>
       <Container fluid>
         <Navbar.Brand href="/">
-          <img src={logo} alt = "logo" style={{ width: "10vh", height: '10vh', borderRadius:"50%" }} />
+          <h1 className='logo-title'> Label <span>23</span></h1>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
