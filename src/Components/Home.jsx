231629c8020b72e9a6dee769c2bd9edab97b3c31
MarkBrian <kariukimarkbrian@gmail.com>
2023-08-17 16:11:03 +0300

    🎨 Imported shheet modal to home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ImageHandler from '../Screens/ImageHandler';
 import Levick from "../Assets/Image/Levick.png";
 import SearchBar from '../Shared/SearchBar';
+import SheetModal from '../Shared/SheetModal';
 
 export default function Home() {
   const imageStyle = {
@@ -13,6 +14,7 @@ export default function Home() {
     <div style={{backgroundColor:"black", minHeight:"100vh"}}>
       <ImageHandler src={Levick} alt="Logo" style={imageStyle} />
       <SearchBar></SearchBar>
+      <SheetModal ></SheetModal>
     </div>
   );
 }
