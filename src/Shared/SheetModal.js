5573a258c5d32e99a13598f5ac228de948d4d94a
MarkBrian <kariukimarkbrian@gmail.com>
2023-08-17 16:11:33 +0300

    💄 impoving m styling on the sheet modal

--- src/Shared/SheetModal.js
+++ src/Shared/SheetModal.js
@@ -11,11 +11,13 @@ export default function SheetModal({ children}) {
         borderTopLeftRadius: "45px",
         borderTopRightRadius: "45px",
         backgroundColor: "black",
+        border: "1px solid goldenrod",
         display: "flex",
         justifyContent: "center",
         height: "100vh",
         overflow: 'hidden',
-        width:"100%"
+        width:"100%",
+        marginTop:"20px"
 
     }}>
       <div>{children}</div>
