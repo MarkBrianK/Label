1c77b17014467aa21eae3ab621321aa1414ccf46
MarkBrian <kariukimarkbrian@gmail.com>
2023-08-17 15:34:46 +0300

    ✨ added logo image to home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ImageHandler from '../Screens/ImageHandler';
+import Levick from "../Assets/Image/Levick.png"
 
-export default function Home(){
+export default function Home() {
+  const imageStyle = {
+    height: "100px",
+    width: "100px"
+  };
 
-  return(
-    <div>
-        <ImageHandler src="image-url.jpg" alt="Description of the image" className="custom-image" />
-
+  return (
+    <div style={{backgroundColor:"black"}}>
+      <ImageHandler src={Levick} alt="Logo" style={imageStyle} />
     </div>
-  )
+  );
 }
