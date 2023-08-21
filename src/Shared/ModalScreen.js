bd02a7b906a52b676359190d57efcd748de9b26a
MarkBrian <kariukimarkbrian@gmail.com>
2023-08-21 13:57:03 +0300

    🎨 structured modal pop up

--- src/Shared/ModalScreen.js
+++ src/Shared/ModalScreen.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { Modal } from "react-bootstrap";
 
-
 function ModalScreen(props) {
-    const { title, body, footer, show, onHide } = props;
-    return (
-        <Modal show={show} onHide={onHide}>
-        <Modal.Header className="modal-header">
-            <Modal.Title className="modal.title">{title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="modal-body">{body}</Modal.Body>
-        <Modal.Footer className="modal.footer">{footer}</Modal.Footer>
+  const { body, footer, show, onHide, name, description, price, size, image } = props;
+
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header className="modal-header">
+      </Modal.Header>
+      <Modal.Body className="modal-body">
+        <div>
+          <img src={image} alt={name} style={{ maxWidth: "100%", maxHeight: "200px" }} />
+        </div>
+        <p><strong>Name:</strong> {name}</p>
+        <p><strong>Description:</strong> {description}</p>
+        <p><strong>Price: ksh</strong> {price}</p>
+        <p><strong>Size:</strong> {size}</p>
+
+      </Modal.Body>
+      <Modal.Footer className="modal.footer">{footer}</Modal.Footer>
     </Modal>
-    )
+  );
 }
-export default ModalScreen
+
+export default ModalScreen;
