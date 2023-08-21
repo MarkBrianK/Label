29539e1abd09902b25ee3a43d56ed85837815f94
MarkBrian <kariukimarkbrian@gmail.com>
2023-08-21 14:22:38 +0300

    💄💄

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -74,6 +74,7 @@ export default function Home() {
                         : filteredClothes
                     }
                     handleViewMore={handleViewMore}
+
                   />
                 </SheetModal>
               )}
--- src/Shared/CardHolder.js
+++ src/Shared/CardHolder.js
@@ -1,32 +1,28 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import Button from './Button';
-
+import Button from "./Button";
+import "../Assets/Styles/CardHolder.css"; // Update the import path
 
 function CardHolder({ cloth, handleViewMore }) {
-
-    return (
-        <Card className="card-container">
-            <Card.Img
-                className="image"
-                variant="top"
-                src={cloth.image}
-                alt={cloth.title}
-            />
-            <Card.Body className="text-center">
-                <Card.Title>{cloth.title}</Card.Title>
-                <Card.Text className="description">
-                    {cloth.description}
-                </Card.Text>
-                <Button
-                    className="custom-button"
-                    onClick={() => handleViewMore(cloth)}
-                >
-                    View More
-                </Button>
-            </Card.Body>
-        </Card>
-    );
+  return (
+    <Card className="card-container">
+      <div className="responsive-image">
+        <Card.Img
+          className="image"
+          variant="top"
+          src={cloth.image}
+          alt={cloth.title}
+        />
+      </div>
+      <Card.Body className="text-center">
+        <Card.Title>{cloth.title}</Card.Title>
+        <Card.Text className="description">{cloth.description}</Card.Text>
+        <Button className="custom-button" onClick={() => handleViewMore(cloth)}>
+          View More
+        </Button>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default CardHolder;
