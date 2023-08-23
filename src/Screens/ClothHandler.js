54391db090ce99bb573993fc9df072033e7296f7
MarkBrian <kariukimarkbrian@gmail.com>
2023-08-23 14:39:02 +0300

    🎨

--- src/Screens/ClothHandler.js
+++ src/Screens/ClothHandler.js
@@ -1,14 +1,19 @@
 import React from "react";
 import CardHolder from "../Shared/CardHolder";
 
-function ClothHandler({ clothes, handleViewMore }) {
+function ClothHandler({ clothes, handleViewMore, selectedCategory }) {
+  // Filter clothes based on the selected category
+  const filteredClothes = selectedCategory
+    ? clothes.filter((cloth) => cloth.category_id === selectedCategory.id)
+    : clothes;
+
   return (
     <div className="cloth-container">
-      {clothes.map((cloth, index) => (
+      {filteredClothes.map((cloth, index) => (
         <CardHolder
           key={index}
           cloth={cloth}
-          handleViewMore={() => handleViewMore(cloth)} 
+          handleViewMore={() => handleViewMore(cloth)}
         />
       ))}
     </div>
