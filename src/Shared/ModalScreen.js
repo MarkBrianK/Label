842645fcfe04dd1cd4ad3adad91d996c6000e4ea
MarkBrian <kariukimarkbrian@gmail.com>
2023-08-23 16:51:47 +0300

    💄

--- src/Shared/ModalScreen.js
+++ src/Shared/ModalScreen.js
@@ -7,18 +7,26 @@ function ModalScreen(props) {
   return (
     <Modal show={show} onHide={onHide}>
       <Modal.Header className="modal-header">
+        {/* Header content */}
       </Modal.Header>
       <Modal.Body className="modal-body">
-        <div>
-          <img src={image} alt={name} style={{ maxWidth: "100%", maxHeight: "200px" }} />
+        <div className="modal-content" style={{display:"flex"}}>
+          {/* Image displayed alone on the left */}
+          <div className="image-container" style={{flex:"1", padding:"10px"}}>
+            <img src={image} alt={name} style={{ maxWidth: "100%", maxHeight: "200px" }} />
+          </div>
+          {/* Other details displayed on the right */}
+          <div className="details-container" style={{flex:"2", padding:"10px"}}>
+            <p><strong>Name:</strong> {name}</p>
+            <p><strong>Description:</strong> {description}</p>
+            <p><strong>Price: ksh</strong> {price}</p>
+            <p><strong>Size:</strong> {size}</p>
+          </div>
         </div>
-        <p><strong>Name:</strong> {name}</p>
-        <p><strong>Description:</strong> {description}</p>
-        <p><strong>Price: ksh</strong> {price}</p>
-        <p><strong>Size:</strong> {size}</p>
-
       </Modal.Body>
-      <Modal.Footer className="modal.footer">{footer}</Modal.Footer>
+      <Modal.Footer className="modal-footer">
+        {footer}
+      </Modal.Footer>
     </Modal>
   );
 }
