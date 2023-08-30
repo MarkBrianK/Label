9df957e6137b60e7dd07579179d93ec8902e3b10
MarkBrian <kariukimarkbrian@gmail.com>
2023-08-30 21:08:29 +0300

    🎨 improving my carousel functionality

--- src/Screens/Carousel.js
+++ src/Screens/Carousel.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Carousel(){
-    return(
-        <div></div>
-    )
+import '../Assets/Styles/Carousel.css'
+
+export default function Carousel({ newArrivals }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    // Auto-slide the carousel
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % newArrivals.length);
+    }, 3000); // Change slide every 3 seconds
+
+    return () => clearInterval(interval);
+  }, [newArrivals]);
+
+  return (
+    <div className="carousel-container">
+      {newArrivals.map((item, index) => (
+        <div
+          key={item.id}
+          className={`carousel-item ${index === currentIndex ? "active" : ""}`}
+        >
+          <img src={item.image} alt={item.name} />
+        </div>
+      ))}
+    </div>
+  );
 }
