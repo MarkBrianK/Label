2555b364bf9346d9649ca1bfd6d1f04d317eb857
MarkBrian <kariukimarkbrian@gmail.com>
2023-09-01 15:22:45 +0300

    🎨

--- src/Screens/Carousel.js
+++ src/Screens/Carousel.js
@@ -1,34 +1,57 @@
 import React, { useState, useEffect } from "react";
-import '../Assets/Styles/Carousel.css'
+import { Carousel as BootstrapCarousel } from "react-bootstrap";
 
 export default function Carousel({ newArrivals }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    // Auto-slide the carousel
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % newArrivals.length);
-    }, 3000); // Change slide every 3 seconds
+    }, 10000);
 
-    return () => clearInterval(interval);
+    // Clear the interval and reset the currentIndex when the component unmounts
+    return () => {
+      clearInterval(interval);
+      setCurrentIndex(0);
+    };
   }, [newArrivals]);
 
+  const carouselStyle = {
+    overflow: "hidden",
+    width: "100%",
+    height: "100%",
+    position: "relative",
+    marginBottom:"20px"
+  };
+
+  const imageStyle = {
+    width: "auto",
+    maxHeight: "80px", // Set a specific height for your carousel images
+    objectFit: "contain",
+    borderRadius: "12px",
+  };
+
   return (
-    <div className="carousel-container">
+    <BootstrapCarousel
+      activeIndex={currentIndex}
+      onSelect={() => {}}
+      controls={false} // Set controls to false to remove arrows
+      indicators={false} // Set indicators to false to remove page indicators
+      style={carouselStyle} // Apply carousel styles
+    >
       {newArrivals.map((item, index) => (
-        <div
-          key={item.id}
-          className={`carousel-item ${index === currentIndex ? "active" : ""}`}
-        >
+        <BootstrapCarousel.Item key={item.id}>
           {item.images.map((imageUrl, imageIndex) => (
             <img
               key={imageIndex}
               src={imageUrl}
               alt={`${item.name}`}
+              className="d-block w-100"
+              style={imageStyle} // Apply image styles
             />
           ))}
-        </div>
+        </BootstrapCarousel.Item>
       ))}
-    </div>
+    </BootstrapCarousel>
   );
 }
