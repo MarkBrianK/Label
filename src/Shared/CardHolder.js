945f92c210b948328f9bceda204975d015df4b50
MarkBrian <kariukimarkbrian@gmail.com>
2023-09-01 16:30:29 +0300

    🚧  working on card images structure

--- src/Shared/CardHolder.js
+++ src/Shared/CardHolder.js
@@ -1,43 +1,71 @@
 import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 import Button from "./Button";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "../Assets/Styles/CardHolder.css";
 
 function CardHolder({ cloth, handleViewMore }) {
   const [showImages, setShowImages] = useState(false);
-  const imageUrls = cloth.image.split(',');
+  const imageUrls = cloth.image.split(",");
 
   const showAdditionalImages = () => {
     setShowImages(true);
   };
 
+  const hideAdditionalImages = () => {
+    setShowImages(false);
+  };
+
+  const sliderSettings = {
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    horizontal: true,
+  };
+
+  const imageStyle = {
+    maxWidth: "100%",
+    maxHeight: "100%",
+  };
+
   return (
     <Card className="card-container">
       <div className="responsive-image">
-        {showImages ? (
-          imageUrls.map((imageUrl, index) => (
-            <img
-              key={index}
-              className="image"
-              src={imageUrl}
-              alt={`${cloth.name}`}
-              style={{ width: `${100 / imageUrls.length}%`, height: `${100 / imageUrls.length}%` }}
-            />
-          ))
-        ) : (
-          <div onClick={showAdditionalImages} className="show-more-overlay">
-            <img
-              className="image"
-              src={imageUrls[0]}
-              alt={`${cloth.name}`}
-              style={{ width: `${100 / imageUrls.length}%`, height: `${100 / imageUrls.length}%` }}
-            />
-            {imageUrls.length > 1 && <div className="overlay-text">+{imageUrls.length - 1}</div>}
+        <img
+          className="image"
+          src={imageUrls[0]}
+          alt={`${cloth.name}`}
+          style={{ ...imageStyle, width: "auto" }}
+        />
+        {showImages && (
+          <div className="image-popup" onClick={hideAdditionalImages}>
+            <Slider {...sliderSettings}>
+              {imageUrls.map((imageUrl, index) => (
+                <div key={index} className="popup-image-container">
+                  <img
+                    className="popup-image"
+                    src={imageUrl}
+                    alt={`${cloth.name}`}
+                    style={imageStyle}
+                  />
+                </div>
+              ))}
+            </Slider>
           </div>
         )}
       </div>
+      {imageUrls.length > 1 && !showImages && (
+        <div className="overlay-text" onClick={showAdditionalImages}>
+          +{imageUrls.length - 1}
+        </div>
+      )}
       <Card.Body className="text-center">
-        <Card.Title style={{ fontSize: "small", fontWeight: "600" }}>{cloth.name}</Card.Title>
+        <Card.Title style={{ fontSize: "small", fontWeight: "600" }}>
+          {cloth.name}
+        </Card.Title>
         <Button className="custom-button" onClick={() => handleViewMore(cloth)}>
           View More
         </Button>
