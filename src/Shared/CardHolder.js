81156c2b2c438ed1c86860a8cc214be0cfca3864
MarkBrian <kariukimarkbrian@gmail.com>
2023-09-01 15:54:00 +0300

    🎨 improving images structure

--- src/Shared/CardHolder.js
+++ src/Shared/CardHolder.js
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 import Button from "./Button";
 import "../Assets/Styles/CardHolder.css";
 
 function CardHolder({ cloth, handleViewMore }) {
-  // Split the image string into an array of image URLs
+  const [showImages, setShowImages] = useState(false);
+
+  
   const imageUrls = cloth.image.split(',');
+
+  const showAdditionalImages = () => {
+    setShowImages(true);
+  };
 
   return (
     <Card className="card-container">
       <div className="responsive-image">
-        {imageUrls.map((imageUrl, index) => (
-          <img
-            key={index}
-            className="image"
-            src={imageUrl}
-            alt={`${cloth.name}`}
-            style={{ width: `${100 / imageUrls.length}%`, height: `${100/ imageUrls.length}%`  }} // Evenly distribute width
-          />
-        ))}
+        {showImages ? (
+          imageUrls.map((imageUrl, index) => (
+            <img
+              key={index}
+              className="image"
+              src={imageUrl}
+              alt={`${cloth.name}`}
+              style={{ width: `${100 / imageUrls.length}%`, height: `${100 / imageUrls.length}%` }}
+            />
+          ))
+        ) : (
+          <div onClick={showAdditionalImages} className="show-more-overlay">
+            <img
+              className="image"
+              src={imageUrls[0]}
+              alt={`${cloth.name}`}
+              style={{ width: `${100 / imageUrls.length}%`, height: `${100 / imageUrls.length}%` }}
+            />
+            <div className="overlay-text">+{imageUrls.length - 1}</div>
+          </div>
+        )}
       </div>
       <Card.Body className="text-center">
-        <Card.Title style={{fontSize:"small", fontWeight:"600"}}>{cloth.name}</Card.Title>
+        <Card.Title style={{ fontSize: "small", fontWeight: "600" }}>{cloth.name}</Card.Title>
         <Button className="custom-button" onClick={() => handleViewMore(cloth)}>
           View More
         </Button>
