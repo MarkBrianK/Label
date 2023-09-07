1af6279915d39ec173a085233fcb878328bded73
MarkBrian <kariukimarkbrian@gmail.com>
2023-09-07 13:42:04 +0300

    🔒️ fixing auth issues

--- src/index.js
+++ src/index.js
@@ -6,16 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import axios from 'axios';
 
-axios.get("http://127.0.0.1:3000/csrfToken")
-  .then(response => {
-    const csrfToken = response.data.csrfToken;
-    axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
-  })
-  .catch(error => {
-    console.error(error);
-  });
+
+
 
 ReactDOM.render(
   <React.StrictMode>
