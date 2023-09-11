915192b9064abab68513043a4abb35b48a3026ca
MarkBrian <kariukimarkbrian@gmail.com>
2023-09-11 20:25:38 +0300

    🐛

--- src/Screens/ClothHandler.js
+++ src/Screens/ClothHandler.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CardHolder from "../Shared/CardHolder";
+import LikeButton from "./LikesHandler";
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -10,22 +11,28 @@ function shuffleArray(array) {
 }
 
 function ClothHandler({ clothes, handleViewMore, selectedCategory }) {
+
+
   // Filter clothes based on the selected category
   const filteredClothes = selectedCategory
     ? clothes.filter((cloth) => cloth.category_id === selectedCategory.id)
     : clothes;
 
+    
   // Shuffle the filtered clothes array
   const shuffledClothes = shuffleArray([...filteredClothes]);
 
   return (
     <div className="cloth-container">
-      {shuffledClothes.map((cloth, index) => (
+      {shuffledClothes.map((item, index) => (
         <CardHolder
           key={index}
-          cloth={cloth}
-          handleViewMore={() => handleViewMore(cloth)}
-        />
+          cloth={item}
+          handleViewMore={() => handleViewMore(item)}
+        >
+        <LikeButton cloth={item} />
+        </CardHolder>
+
       ))}
     </div>
   );
