55c296da6cc4f6c63dc4ce3387453d0169ea69fb
MarkBrian <kariukimarkbrian@gmail.com>
2023-09-14 13:06:52 +0300

    🎨

--- src/Screens/ClothHandler.js
+++ src/Screens/ClothHandler.js
@@ -1,7 +1,8 @@
 import React from "react";
 import CardHolder from "../Shared/CardHolder";
 import LikeButton from "./LikesHandler";
-// import CommentHandler from "./CommentsHandler";
+import { Link } from "react-router-dom";
+import { FaComment } from "react-icons/fa";
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -11,14 +12,11 @@ function shuffleArray(array) {
   return array;
 }
 
-function ClothHandler({ clothes, handleViewMore, selectedCategory }) {
-
-
+function ClothHandler({ clothes, handleViewMore, selectedCategory, isLoggedIn }) {
   // Filter clothes based on the selected category
   const filteredClothes = selectedCategory
     ? clothes.filter((cloth) => cloth.category_id === selectedCategory.id)
     : clothes;
-
 
   // Shuffle the filtered clothes array
   const shuffledClothes = shuffleArray([...filteredClothes]);
@@ -31,9 +29,19 @@ function ClothHandler({ clothes, handleViewMore, selectedCategory }) {
           cloth={item}
           handleViewMore={() => handleViewMore(item)}
         >
-        <LikeButton cloth={item} />
+          <div style={{ display: "flex", alignItems: "center", gap: "20%", marginLeft: "30%" }}>
+            <LikeButton cloth={item} />
+            {isLoggedIn ? (
+              <Link style={{ textDecoration: "none", display: "flex", alignItems: "center", color: "black" }} to={`comments/${item.id}`} className="comment-link ml-2">
+                <FaComment /> {item.comments.length}
+              </Link>
+            ) : (
+              <span style={{ color: "black" }}>
+                <FaComment /> {item.comments.length}
+              </span>
+            )}
+          </div>
         </CardHolder>
-
       ))}
     </div>
   );
