4249d27fa5357873b6cb54ecb80fb785366ff42c
MarkBrian <kariukimarkbrian@gmail.com>
2023-09-19 16:01:25 +0300

    🐛 fixing rotes names

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,8 +1,13 @@
-export const ROUTES = {
-    home:"/",
-    signUp:"/signup",
-    signIn:"/signin",
-    clothcomments:"comments/:clothId",
-    profile: "profile/:userId"
+const home = "/";
+const signup = "/signup";
+const signin = "/signin";
+const comments = "/comments/:clothId";
+const profile = "/profile/:userId";
 
+export const ROUTES = {
+  home: home,
+  signUp: signup,
+  signIn: signin,
+  comments: comments,
+  profile: profile
 }
