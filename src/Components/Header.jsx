a95170606623fce248477d75c1cc56a72af83d0c
MarkBrian <kariukimarkbrian@gmail.com>
2023-09-19 16:03:12 +0300

    🐛 fixing profile link

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { Image } from 'react-bootstrap';
-import { AccountCircle, ExitToApp, Home, Mail, ShoppingBag } from '@mui/icons-material';
-import '../Assets/Styles/Header.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { Image } from "react-bootstrap";
+import { AccountCircle, Home, Mail, ShoppingBag } from "@mui/icons-material";
+import "../Assets/Styles/Header.css";
 
-function Header({ user }) {
-  const sessionCookie = localStorage.getItem('session_id');
-  const navigate = useNavigate();
+function Header({ user  }) {
+  const sessionCookie = localStorage.getItem("session_id");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [profilePicture, setProfilePicture] = useState(null);
 
@@ -15,40 +14,19 @@ function Header({ user }) {
     setIsLoggedIn(!!sessionCookie);
 
     if (!!sessionCookie) {
-      axios.get(`http://127.0.0.1:3000/users/${user}`)
-        .then(response => {
+      axios
+        .get(`http://127.0.0.1:3000/users/${user}`)
+        .then((response) => {
           setProfilePicture(response.data.profile_picture);
         })
-        .catch(error => console.error('Error fetching profile picture:', error));
+        .catch((error) =>
+          console.error("Error fetching profile picture:", error)
+        );
     }
   }, [sessionCookie, user]);
 
-  const handleAuth = async (e) => {
-    e.preventDefault();
-    if (isLoggedIn) {
-      try {
-        await axios.delete('http://127.0.0.1:3000/users/sign_out', {
-          headers: {
-            Authorization: `Bearer ${sessionCookie}`,
-          },
-          withCredentials: true,
-        });
-
-        localStorage.removeItem('session_id');
-        localStorage.removeItem('user_id');
-        setIsLoggedIn(false);
-        window.location.reload();
-        navigate('/');
-      } catch (error) {
-        console.error(error);
-      }
-    } else {
-      navigate('/');
-    }
-  };
-
   return (
-    <header className={`header ${isLoggedIn ? 'loggedIn' : ''}`}>
+    <header className={`header ${isLoggedIn ? "loggedIn" : ""}`}>
       <div className="nav-icons">
         <Link to="/" className="icon-link">
           <Home className="icon" />
@@ -57,25 +35,44 @@ function Header({ user }) {
         <Link to="/" className="icon-link">
           <ShoppingBag className="icon" />
         </Link>
+
         <Link to="/" className="icon-link">
           <Mail className="icon" />
         </Link>
+
         <div className="auth-link">
           {isLoggedIn ? (
-            <div >
-              {profilePicture ? (
-                <Image
-                  src={profilePicture}
-                  roundedCircle
-                  width={50}
-                  height={50}
-                  className="profile-picture"
-                />
-              ) : (
-                <AccountCircle className="icon" />
-              )}
-              <ExitToApp onClick={handleAuth} className="icon" />
-            </div>
+            <Link to={`/profile/${user}`} className="icon-link">
+              <div
+                className="d-flex align-items-center"
+                style={{
+                  borderRadius: "50%",
+                  cursor: "pointer",
+                  border: "1px solid goldenrod",
+                  marginRight: "10px",
+                  width: "40px",
+                  height: "40px",
+                  minWidth: "40px",
+                  minHeight: "40px",
+                  padding: 0,
+                }}
+              >
+                {profilePicture ? (
+                  <Image
+                    src={profilePicture}
+                    roundedCircle
+                    alt={user}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      borderRadius: "50%",
+                    }}
+                  />
+                ) : (
+                  <AccountCircle className="icon" />
+                )}
+              </div>
+            </Link>
           ) : (
             <Link to="/signin" className="icon-link">
               <AccountCircle className="icon" />
