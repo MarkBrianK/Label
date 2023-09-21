ed22568316affa15e5f1d3ca58e2d02c1ca27c29
MarkBrian <kariukimarkbrian@gmail.com>
2023-09-22 00:08:28 +0300

    🎨 improve profile page logic

--- src/App.js
+++ src/App.js
@@ -16,18 +16,22 @@ function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState(null)
+  const [userdetails, setUserDetails] = useState(null)
 
   useEffect(() => {
     try {
       const secretKey = "wabebee_x1_levick";
       const encryptedUserID = localStorage.getItem("user_id");
+      setUserDetails(localStorage.getItem("username"))
       if (encryptedUserID) {
+
         const bytes = CryptoJS.AES.decrypt(encryptedUserID, secretKey);
         const decryptedUserData = bytes.toString(CryptoJS.enc.Utf8);
 
         if (decryptedUserData) {
           const currentUser = JSON.parse(decryptedUserData);
           setUser(currentUser);
+
         } else {
 
           console.error("Please log in.");
@@ -57,10 +61,10 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path={ROUTES.home} element={<Suspense fallback={<div>Loading...</div>}><Home user={user} isLoggedIn={isLoggedIn} /></Suspense>} />
+        <Route path={ROUTES.home} element={<Suspense fallback={<div>Loading...</div>}><Home user={user} userdetails={userdetails} isLoggedIn={isLoggedIn} /></Suspense>} />
 
         <Route path={ROUTES.clothcomments} element={<Suspense fallback={<div> Loading...</div>}  > <CommentHandler user={user} /></Suspense>} />
-        <Route path={ROUTES.profile} element={<Suspense fallback={<div>Loading ...</div>}> < Profile user={user} /> </Suspense>} />
+        <Route path={ROUTES.profile} element={<Suspense fallback={<div>Loading ...</div>}> < Profile user={user} userdetails={userdetails} /> </Suspense>} />
 
         {/* Render the SignUpFo<Route path=''rm route only when the user is not logged in */}
         {!isLoggedIn && (
