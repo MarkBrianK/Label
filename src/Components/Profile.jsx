3d2837a88cedf55dcfa2feecf954d298de36d493
MarkBrian <kariukimarkbrian@gmail.com>
2023-09-23 23:12:15 +0300

    adding edit profile functionality

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -40,35 +40,6 @@ export default function Profile({ user, userdetails}) {
 
 
 
-  const handleUpdateProfile = async () => {
-    if (!user) {
-      console.error("User is null. Cannot update profile.");
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append("user[username]", username);
-    if (profilePicture) {
-      formData.append("user[profile_picture]", profilePicture);
-    }
-
-    try {
-      await axios.patch(
-        `https://levick-7b15defb7ee9.herokuapp.com/users/${user}`,
-        formData
-      );
-      console.log("Profile updated successfully.");
-
-      // Reload the page after updating the profile
-      window.location.reload();
-    } catch (error) {
-      console.error("Error updating profile:", error);
-    }
-  };
-
-  const handleFileChange = (event) => {
-    setProfilePicture(event.target.files[0]);
-  };
   const handleAuth = async () => {
 
     if (isLoggedIn) {
