bd14f62df1555a824d19f9209890d0ddce41c585
MarkBrian <kariukimarkbrian@gmail.com>
2023-09-28 15:23:21 +0300

    added a camera icon on top of profile image

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -8,6 +8,7 @@ export default function Profile({ user, userdetails }) {
   const [username, setUsername] = useState("");
   const [profilePicture, setProfilePicture] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const sessionCookie = localStorage.getItem("session_id");
   const navigate = useNavigate();
 
@@ -64,12 +65,43 @@ export default function Profile({ user, userdetails }) {
         <div className="col-md-4">
           <div className="text-center">
             {profilePicture && (
-              <img
-                src={profilePicture}
-                alt="Profile_picture"
-                className="img-fluid rounded-circle"
-                style={{ width: "100px", height: "100px" }}
-              />
+              <div
+                style={{
+                  position: "relative",
+                  width: "100px",
+                  height: "100px",
+                  cursor: "pointer",
+                }}
+                onClick={() => setShowModal(true)}
+              >
+                <img
+                  src={profilePicture}
+                  alt="Profile_picture"
+                  className="img-fluid rounded-circle"
+                  style={{ width: "100%", height: "100%" }}
+                />
+                <div
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    color: "white",
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-camera"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z" />
+                    <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />
+                  </svg>
+                </div>
+              </div>
             )}
           </div>
         </div>
@@ -77,16 +109,42 @@ export default function Profile({ user, userdetails }) {
         <div className="col-md-8">
           <div className="user-details">
             <h3 className="font-weight-bold">{username}</h3>
-            <div
-              className="button-container mt-3"
-              style={{ display: "flex", gap: "10px" }}
-            >
+            <div className="button-container mt-3 d-flex gap-2">
               <Button onClick={handleAuth}>Log Out</Button>
               <Button onClick={handleProfileEdit}>Edit Profile</Button>
             </div>
           </div>
         </div>
       </div>
+
+      {profilePicture && (
+        <div
+          className={`modal fade ${showModal ? "show" : ""}`}
+          style={{ display: showModal ? "block" : "none" }}
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-body text-center">
+                <img
+                  src={profilePicture}
+                  alt="Profile_picture"
+                  className="img-fluid"
+                  style={{ maxWidth: "100%", maxHeight: "80vh" }}
+                />
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={() => setShowModal(false)}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
 
       <div className="row mt-4">
         <div className="col-md-12">
