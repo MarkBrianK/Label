f1cb8d88459f2e4ae432c8ebad1cb020a182c35e
MarkBrian <kariukimarkbrian@gmail.com>
2023-10-02 12:15:32 +0300

    addd icon in case user has not added any profile image

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { AccountCircle } from "@mui/icons-material";
 import axios from "axios";
 import Header from "./Header";
 import Button from "../Shared/Button";
@@ -70,55 +71,69 @@ export default function Profile({ user, userdetails }) {
       <div className="row">
         <div className="col-md-4">
           <div className="text-center">
-            {profilePicture && (
-              <div
-                style={{
-                  position: "relative",
-                  width: "100px",
-                  height: "100px",
-                  cursor: "pointer",
-                  marginTop: "10px",
-                }}
-                onClick={() => setShowModal(true)}
-              >
-                <img
-                  src={profilePicture}
-                  alt="Profile_picture"
-                  className="img-fluid rounded-circle"
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
-                  }}
-                />
-                <div
-                  style={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                    color: "white",
-                  }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-camera"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z"
-                    />
-                    <path
-                      d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"
-                    />
-                  </svg>
-                </div>
-              </div>
-            )}
-
+          {profilePicture ? (
+        <div
+          style={{
+            position: "relative",
+            width: "100px",
+            height: "100px",
+            cursor: "pointer",
+            marginTop: "10px",
+          }}
+          onClick={() => setShowModal(true)}
+        >
+          {/* Profile picture */}
+          <img
+            src={profilePicture}
+            alt="Profile_picture"
+            className="img-fluid rounded-circle"
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+            }}
+          />
+          {/* Camera icon */}
+          <div
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              color: "white",
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-camera"
+              viewBox="0 0 16 16"
+            >
+              {/* Camera icon paths */}
+            </svg>
+          </div>
+        </div>
+      ) : (
+        // Render AccountCircle when there's no profile picture
+        <div
+          style={{
+            width: "100px",
+            height: "100px",
+            marginTop: "10px",
+          }}
+        >
+          <AccountCircle
+            style={{
+              width: "100%",
+              height: "100%",
+              borderRadius: "50%",
+              objectFit: "cover",
+            }}
+          />
+        </div>
+      )}
           </div>
         </div>
         <div className="col-md-8">
