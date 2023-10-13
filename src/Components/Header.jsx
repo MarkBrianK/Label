01ec9d8f15113e3ddb8c6ad3d70d6471943e3bb7
MarkBrian <kariukimarkbrian@gmail.com>
2023-10-13 16:55:44 +0300

    change backend route

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -16,7 +16,7 @@ function Header({ user, username }) {
 
     if (!!sessionCookie) {
       axios
-        .get(`https://levick-7b15defb7ee9.herokuapp.com/users/${user}`)
+        .get(`https://seal-app-p8ntf.ondigitalocean.app/users/${user}`)
         .then((response) => {
           setProfilePicture(response.data.profile_picture);
         })
