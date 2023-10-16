9e7be7db45681241a291bff172ebb30153f0164f
MarkBrian <kariukimarkbrian@gmail.com>
2023-10-16 10:39:24 +0300

    add  account confirmation page

---
+++ src/Screens/ConfirmationPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ConfirmationPage() {
+    return (
+        <div>
+            <p>
+
+                You have successfully created your account. please check Your email to confirm your account
+            </p>
+
+
+        </div>
+    )
+}
