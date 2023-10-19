a34c2b8c8ce9c08fbf8ddf47a4e1d8032720c732
MarkBrian <kariukimarkbrian@gmail.com>
2023-10-19 21:56:53 +0300

    Added admin coth handler

---
+++ src/Admin/ClothHandler/ClothHandler.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function ClothHandler(){
+    return(
+        <div>
+            <p> Admin cloth handler with all function</p>
+        </div>
+    )
+}
