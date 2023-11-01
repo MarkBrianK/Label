9ec69143521c67cee5565f2221973da6976f4121
MarkBrian <kariukimarkbrian@gmail.com>
2023-11-01 20:25:13 +0300

    :bug: fix all saales route

--- src/Screens/MakeSale.js
+++ src/Screens/MakeSale.js
@@ -56,7 +56,7 @@ export default function MakeSaleForm({ user }) {
       });
 
       if (response.ok) {
-        navigate('/allsales');
+        navigate('/all_sales');
       } else {
         console.error("Error submitting the sale:", response.status, response.statusText);
       }
