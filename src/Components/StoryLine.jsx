90c0463287113e69407d7b31ce9935948eee2a53
MarkBrian <kariukimarkbrian@gmail.com>
2023-11-01 15:50:28 +0300

    :sparkles: implement top sales

--- src/Components/StoryLine.jsx
+++ src/Components/StoryLine.jsx
@@ -1,8 +1,45 @@
 import React from "react";
+import StarRating from "../Screens/StarRating";
 
-export default function StoryLine(){
-    return(
-        <div>
-        </div>
-    )
+export default function StoryLine({ clothes}) {
+  // Function to calculate the rating based on sales
+  const calculateRatingFromSales = (sales) => {
+    if (sales >= 100) {
+      return 5; // Highest rating for items with 100 or more sales
+    } else if (sales >= 50) {
+      return 4; // High rating for items with 50 or more sales
+    } else if (sales >= 20) {
+      return 3; // Moderate rating for items with 20 or more sales
+    } else if (sales >= 10) {
+      return 2; // Low rating for items with 10 or more sales
+    } else if (sales>=1){
+      return 1;
+       // Lowest rating for items with fewer than 10 sales
+    }
+    else{
+      return 0;
+    }
+
+  };
+
+  return (
+    <div>
+      <h1>Top Sales</h1>
+      <ul>
+        {clothes.map((clothing) => (
+          <li key={clothing.id}>
+            <div>
+              <h3>{clothing.name}</h3>
+              <p>Description: {clothing.description}</p>
+              <p>Price: ${clothing.price}</p>
+              <p>Size: {clothing.size}</p>
+              <p>Category: {clothing.category.name}</p>
+              <p>Sales: {clothing.sales.length}</p>
+              <p>Rating: <StarRating rating={calculateRatingFromSales(clothing.sales.length)} /></p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
