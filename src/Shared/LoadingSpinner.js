c8b47da6878024531a85e911b85b58e267083622
MarkBrian <kariukimarkbrian@gmail.com>
2023-11-06 00:23:19 +0300

    :art: fix isloading

---
+++ src/Shared/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "../Assets/Styles/LoadingSpinner.css"
+
+export default function LoadingSpinner(){
+    return(
+        <div className="loading-spinner">
+              <div className="spinner"></div>
+            </div>
+    )
+}
