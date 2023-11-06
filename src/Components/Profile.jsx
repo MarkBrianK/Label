6ade5c705aeee43fec439ec1e29c766b9adeced0
MarkBrian <kariukimarkbrian@gmail.com>
2023-11-06 14:35:26 +0300

    :lipstick: improve user experience

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -74,14 +74,14 @@ export default function Profile({ user, userdetails }) {
   };
 
   return (
-<div className={`${Styles.containerMain} mt-2`}>
+    <div className={`${Styles.containerMain} mt-2`}>
       <label
         className="col-md-4 mt-2 d-flex justify-content-center align-items-center"
         style={{
           fontWeight: "bold",
           borderBottom: "1px solid #000",
           width: "100%",
-          lineHeight:" 2.5"
+          lineHeight: " 2.5"
         }}
       >
         My Profile
@@ -102,125 +102,129 @@ export default function Profile({ user, userdetails }) {
       </Helmet>
       <div className="column" style={{ minHeight: "100%" }}>
         <div className="col-md-12 mt-1 d-flex justify-content-center align-items-center">
-          <div className="text-center">
-            {profilePicture ? (
+        <div className="text-center">
+          {profilePicture ? (
+            <div
+              style={{
+                position: "relative",
+                width: "200px",
+                height: "200px",
+                borderRadius: "50%",
+                overflow: "hidden",
+                cursor: "pointer",
+              }}
+              onClick={() => setShowModal(true)}
+            >
+              <img
+                src={profilePicture}
+                alt="Profile_picture"
+                className="profileimage"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
               <div
                 style={{
-                  position: "relative",
-                  width: "200%",
-                  height: "200%",
-                  cursor: "pointer",
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  color: "white",
                 }}
-                onClick={() => setShowModal(true)}
               >
-                <img
-                  src={profilePicture}
-                  alt="Profile_picture"
-                  className="profileimage"
-                  style={{
-                    width: "200%",
-                    height: "200%",
-                    objectFit: "cover",
-                  }}
-                />
-                <div
-                  style={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                    color: "white",
-                  }}
+                <svg
+                  xmlns={camera}
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
                 >
-                  <svg
-                    xmlns={camera}
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    viewBox="0 0 16 16"
-                  >
-                    {/* Camera icon */}
-                  </svg>
-                </div>
+                  {/* Camera icon */}
+                </svg>
               </div>
-            ) : (
-              <div
+            </div>
+          ) : (
+            <div
+              style={{
+                width: "100px",
+                height: "100px",
+                marginTop: "10px",
+                borderRadius: "50%",
+                overflow: "hidden",
+              }}
+            >
+              <AccountCircle
                 style={{
-                  width: "100px",
-                  height: "100px",
-                  marginTop: "10px",
+                  width: "100%",
+                  height: "100%",
+                  borderRadius: "50%",
+                  objectFit: "cover",
                 }}
-              >
-                <AccountCircle
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    borderRadius: "50%",
-                    objectFit: "cover",
-                  }}
-                />
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="col-md-12 text-center">
-          <div className="user-details mt-3">
-            <h3 className={` ${Styles.userInfo}`}>{username}</h3>
-            <div className={`font-weight-bold mt-3 ${Styles.userInfo}`}>
-              Mobile Number: <span>{mobileNumber}</span>
+              />
             </div>
-            <div className={`font-weight-bold mt-3 ${Styles.userInfo}`}>
-              County: <span>{county}</span>
-            </div>
-            <div className={`font-weight-bold mt-3 ${Styles.userInfo}`}>
-              Email: <span>{email}</span>
-            </div>
-            <div className="button-container d-flex justify-content-center col-md-12 gap-2">
-              <button onClick={handleProfileEdit} className={Styles.profileButton}>
-                Edit Profile
-              </button>
-              <button onClick={handleAuth} className={Styles.profileButton}>
-                Log Out
-              </button>
-            </div>
-          </div>
+          )}
         </div>
       </div>
-      {
-        profilePicture && (
-          <div
-            className={`modal fade ${showModal ? "show" : ""}`}
-            style={{ display: showModal ? "block" : "none" }}
-          >
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-body text-center">
-                  <img
-                    src={profilePicture}
-                    alt="Profile_picture"
-                    className="img-fluid"
-                    style={{ maxWidth: "100%", maxHeight: "80vh" }}
-                  />
-                </div>
-                <div className="modal-footer">
-                  <Button
-                    type="button"
-                    className="btn btn-secondary"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </Button>
-                </div>
-              </div>
-            </div>
+      <div className="col-md-12 text-center">
+        <div className="user-details mt-3">
+          <h3 className={` ${Styles.userInfo}`}>{username}</h3>
+          <div className={`font-weight-bold mt-3 ${Styles.userInfo}`}>
+            Mobile Number: <span>{mobileNumber}</span>
           </div>
-        )
-      }
-      <div>
-        <div className="">
-          <Header username={userdetails} user={user} />
+          <div className={`font-weight-bold mt-3 ${Styles.userInfo}`}>
+            County: <span>{county}</span>
+          </div>
+          <div className={`font-weight-bold mt-3 ${Styles.userInfo}`}>
+            Email: <span>{email}</span>
+          </div>
+          <div className="button-container d-flex justify-content-center col-md-12 gap-2">
+            <button onClick={handleProfileEdit} className={Styles.profileButton}>
+              Edit Profile
+            </button>
+            <button onClick={handleAuth} className={Styles.profileButton}>
+              Log Out
+            </button>
+          </div>
         </div>
       </div>
     </div>
+      {
+    profilePicture && (
+      <div
+        className={`modal fade ${showModal ? "show" : ""}`}
+        style={{ display: showModal ? "block" : "none" }}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-body text-center">
+              <img
+                src={profilePicture}
+                alt="Profile_picture"
+                className="img-fluid"
+                style={{ maxWidth: "100%", maxHeight: "80vh" }}
+              />
+            </div>
+            <div className="modal-footer">
+              <Button
+                type="button"
+                className="btn btn-secondary"
+                onClick={() => setShowModal(false)}
+              >
+                Close
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  <div>
+    <div className="">
+      <Header username={userdetails} user={user} />
+    </div>
+  </div>
+    </div >
   );
 }
