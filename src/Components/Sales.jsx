550a4fa5d9450c396bac40eb6875f3a0d149f8f8
Kariuki Karanja <kariukimarkbrian@gmail.com>
2023-11-08 13:02:49 +0300

    :lipstick:

--- src/Components/Sales.jsx
+++ src/Components/Sales.jsx
@@ -4,7 +4,7 @@ import { TextField } from "@mui/material";
 import { Table, TableHead, TableBody, TableRow, TableCell } from "@mui/material";
 import Header from "./Header";
 import MakeSaleForm from "../Screens/MakeSale";
-import styles from "../Assets/Styles/Sales.module.css"; 
+import styles from "../Assets/Styles/Sales.module.css";
 
 export default function Sales({ clothes, user }) {
   const navigate = useNavigate();
@@ -31,69 +31,72 @@ export default function Sales({ clothes, user }) {
   };
 
   return (
-    <div className={styles.Container}>
-      <TextField
-        label="Search"
-        variant="outlined"
-        fullWidth
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        className={styles.searchInput}
-      />
-      <div className={styles.tableContainer}>
-        <Table className={styles.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Image</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Description</TableCell>
-              <TableCell>Price</TableCell>
-              <TableCell>Category</TableCell>
-              <TableCell>Make a Sale</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {filteredClothes.map((cloth) => {
-              const imageUrls = JSON.parse(cloth.image);
-              const firstImageUrl = imageUrls.length > 0 ? imageUrls[0] : null;
-              const clothId = cloth.id;
-
-              return (
-                <TableRow key={cloth.id}>
-                  <TableCell>
-                    {firstImageUrl && (
-                      <img
-                        src={firstImageUrl}
-                        alt={cloth.name}
-                        style={{ width: "100px", height: "100px" }}
-                      />
-                    )}
-                  </TableCell>
-                  <TableCell>{cloth.name}</TableCell>
-                  <TableCell>{cloth.description}</TableCell>
-                  <TableCell>Ksh {cloth.price}/=</TableCell>
-                  <TableCell>{cloth.category.name}</TableCell>
-                  <TableCell>
-                    <button
-                      onClick={() => handleMakeSale(clothId)}
-                      className={styles.makeSaleButton}
-                    >
-                      Make a Sale
-                    </button>
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </div>
-      {isMakingSale && selectedCloth && (
-        <MakeSaleForm
-          selectedCloth={selectedCloth}
-          onSaleSubmit={handleSaleSubmit}
-          onCancel={handleCancelSale}
+    <div>
+      <div className={styles.Container}>
+        <TextField
+          label="Search"
+          variant="outlined"
+          fullWidth
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className={styles.searchInput}
         />
-      )}
+        <div className={styles.tableContainer}>
+          <Table className={styles.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Image</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Description</TableCell>
+                <TableCell>Price</TableCell>
+                <TableCell>Category</TableCell>
+                <TableCell>Make a Sale</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredClothes.map((cloth) => {
+                const imageUrls = JSON.parse(cloth.image);
+                const firstImageUrl = imageUrls.length > 0 ? imageUrls[0] : null;
+                const clothId = cloth.id;
+
+                return (
+                  <TableRow key={cloth.id}>
+                    <TableCell>
+                      {firstImageUrl && (
+                        <img
+                          src={firstImageUrl}
+                          alt={cloth.name}
+                          style={{ width: "100px", height: "100px", objectFit:"cover" }}
+                        />
+                      )}
+                    </TableCell>
+                    <TableCell>{cloth.name}</TableCell>
+                    <TableCell>{cloth.description}</TableCell>
+                    <TableCell>Ksh {cloth.price}/=</TableCell>
+                    <TableCell>{cloth.category.name}</TableCell>
+                    <TableCell>
+                      <button
+                        onClick={() => handleMakeSale(clothId)}
+                        className={styles.makeSaleButton}
+                      >
+                        Make a Sale
+                      </button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </div>
+        {isMakingSale && selectedCloth && (
+          <MakeSaleForm
+            selectedCloth={selectedCloth}
+            onSaleSubmit={handleSaleSubmit}
+            onCancel={handleCancelSale}
+          />
+        )}
+
+      </div>
       <Header user={user} />
     </div>
   );
