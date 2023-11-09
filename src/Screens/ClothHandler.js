20c96c6061058c502b1264fa92c0f0cddcbfa826
Kariuki Karanja <kariukimarkbrian@gmail.com>
2023-11-09 14:36:05 +0300

    :art:

--- src/Screens/ClothHandler.js
+++ src/Screens/ClothHandler.js
@@ -3,27 +3,18 @@ import CardHolder from "../Shared/CardHolder";
 import LoadingSpinner from "../Shared/LoadingSpinner"; // Import a loading spinner component
 import "../Assets/Styles/Handler.css";
 
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-function ClothHandler({ clothes, handleViewMore, selectedCategory, user, isLoading }) {
-  const [shuffledClothes, setShuffledClothes] = useState([]);
 
-  useEffect(() => {
-    const newShuffledClothes = shuffleArray([...clothes]);
-    setShuffledClothes(newShuffledClothes);
-  }, [clothes]);
+
+function ClothHandler({ clothes, handleViewMore, selectedCategory, user, isLoading }) {
+
+
 
   return (
     <div className="cloth-container">
       {isLoading ? (
         <LoadingSpinner /> // Display the loading spinner if isLoading is true
       ) : (
-        shuffledClothes.map((item, index) => (
+        clothes.map((item, index) => (
           <CardHolder
             key={index}
             user={user}
