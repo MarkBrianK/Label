833736d993ed35276f38bd1026394c7010f2a075
Kariuki Karanja <kariukimarkbrian@gmail.com>
2023-11-09 14:33:18 +0300

    :art: lazy loading

--- src/Shared/CardHolder.js
+++ src/Shared/CardHolder.js
@@ -5,13 +5,11 @@ import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import Button from "./Button";
 import "../Assets/Styles/CardHolder.css";
 
-
 function CardHolder({ cloth, handleViewMore, user, children }) {
   const imageUrls = JSON.parse(cloth.image);
   const showImageCarousel = imageUrls.length > 1;
 
   const [imagesLoaded, setImagesLoaded] = useState(Array(imageUrls.length).fill(false));
-  //const [isLoading, setIsLoading] = useState(true);
 
   const iconButtonStyle = {
     position: "absolute",
@@ -23,85 +21,70 @@ function CardHolder({ cloth, handleViewMore, user, children }) {
     cursor: "pointer",
   };
 
-  function isWithinLastTwoWeeks(dateString) {
-    const today = new Date();
-    const creationDate = new Date(dateString);
-    const twoWeeksAgo = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
-
-    return creationDate >= twoWeeksAgo;
-  }
-
   const handleImageLoad = (index) => {
     const newImagesLoaded = [...imagesLoaded];
     newImagesLoaded[index] = true;
     setImagesLoaded(newImagesLoaded);
-
-    // Check if all images have loaded
-    // if (!newImagesLoaded.includes(false)) {
-    //   setIsLoading(false); // Set isLoading to false when all images have loaded
-    // }
   };
 
   return (
     <Card className="card-container">
       <div className="responsive-image">
         {showImageCarousel && (
-          <>
-
-            <Carousel
-              showStatus={false}
-              showThumbs={false}
-              swipeable={false}
-              renderArrowPrev={(onClickHandler, hasPrev, label) =>
-                hasPrev && (
-                  <button
-                    type="button"
-                    onClick={onClickHandler}
-                    title={label}
-                    style={{
-                      ...iconButtonStyle,
-                      left: "15px",
-                      color: "white",
-                    }}
-                  >
-                    <IoIosArrowBack size={30} />
-                  </button>
-                )
-              }
-              renderArrowNext={(onClickHandler, hasNext, label) =>
-                hasNext && (
-                  <button
-                    type="button"
-                    onClick={onClickHandler}
-                    title={label}
-                    style={{
-                      ...iconButtonStyle,
-                      right: "15px",
-                      color: "white",
-                    }}
-                  >
-                    <IoIosArrowForward size={30} />
-                  </button>
-                )
-              }
-            >
-              {imageUrls.map((imageUrl, index) => (
-                <div key={index} className="popup-image-container" >
-                  <img
-                    className={`popup-image ${imagesLoaded[index] ? 'loaded' : 'hidden'}`}
-                    src={imageUrl}
-                    alt={`${cloth.name}`}
-                    style={{
-                      width: "100%",
-                      aspectRatio: "1/1",
-                      objectFit: "cover",
-                    }}
-                    onLoad={() => handleImageLoad(index)}
-                  />
-                </div>
-              ))}
-            </Carousel>
-          </>
+          <Carousel
+            showStatus={false}
+            showThumbs={false}
+            swipeable={false}
+            renderArrowPrev={(onClickHandler, hasPrev, label) =>
+              hasPrev && (
+                <button
+                  type="button"
+                  onClick={onClickHandler}
+                  title={label}
+                  style={{
+                    ...iconButtonStyle,
+                    left: "15px",
+                    color: "white",
+                  }}
+                >
+                  <IoIosArrowBack size={30} />
+                </button>
+              )
+            }
+            renderArrowNext={(onClickHandler, hasNext, label) =>
+              hasNext && (
+                <button
+                  type="button"
+                  onClick={onClickHandler}
+                  title={label}
+                  style={{
+                    ...iconButtonStyle,
+                    right: "15px",
+                    color: "white",
+                  }}
+                >
+                  <IoIosArrowForward size={30} />
+                </button>
+              )
+            }
+          >
+            {imageUrls.map((imageUrl, index) => (
+              <div key={index} className="popup-image-container">
+                <img
+                  className={`popup-image ${imagesLoaded[index] ? 'loaded' : 'hidden'}`}
+                  src={imageUrl}
+                  alt={`${cloth.name}`}
+                  style={{
+                    width: "100%",
+                    aspectRatio: "1/1",
+                    objectFit: "cover",
+                  }}
+                  onLoad={() => handleImageLoad(index)}
+                  loading="lazy" // Add the lazy loading attribute
+                />
+              </div>
+            ))}
+          </Carousel>
         )}
         {!showImageCarousel && (
           <img
@@ -115,42 +98,8 @@ function CardHolder({ cloth, handleViewMore, user, children }) {
               borderRadius: "6px 6px 0 0",
             }}
             onLoad={() => handleImageLoad(0)}
+            loading="lazy" // Add the lazy loading attribute
           />
-        )}
-
-        {isWithinLastTwoWeeks(cloth.created_at) && (
-          <div
-            className="new-arrival-mark"
-            style={{
-              position: "absolute",
-              top: "5px",
-              left: "5px",
-              width: "34px",
-              height: "34px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              fontSize: "14px",
-              fontWeight: "bold",
-            }}
-          >
-            <span
-              style={{
-                backgroundColor: "black",
-                color: "goldenrod",
-                borderRadius: "50%",
-                padding: "4px",
-                display: "inline-block",
-                width: "100%",
-                height: "100%",
-                textAlign: "center",
-                lineHeight: "1",
-                fontSize: "x-small",
-              }}
-            >
-              &#9733; New
-            </span>
-          </div>
         )}
       </div>
       <Card.Body className="text-center">
