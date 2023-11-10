3e9e92cd448d8503acbafb5c222bf2060833cfde
Kariuki Karanja <kariukimarkbrian@gmail.com>
2023-11-10 08:58:43 +0300

    :art: improve speed

--- src/App.js
+++ src/App.js
@@ -1,48 +1,37 @@
 import React, { useEffect, useState, lazy, Suspense } from 'react';
-import "./App.css"
+import { CloudinaryContext } from 'cloudinary-react';
+import "./App.css";
 import { Routes, Route } from 'react-router-dom';
 import { ROUTES } from './Routes/Routes';
-import CryptoJS from 'crypto-js'
+import CryptoJS from 'crypto-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Helmet } from "react-helmet";
-import ContactPage from "./Components/ContactPage"
+import { Helmet } from 'react-helmet';
+import ContactPage from './Components/ContactPage';
 import LoadingSpinner from './Shared/LoadingSpinner';
-//import Sales from './Components/Sales';
-//import MakeSaleForm from './Screens/MakeSale';
-
-
-
 
 const Home = lazy(() => import('./Components/Home'));
 const SignUpForm = lazy(() => import('./Auth/Signup'));
 const SignInForm = lazy(() => import('./Auth/Signin'));
 const CommentHandler = lazy(() => import('./Screens/CommentsHandler'));
 const Profile = lazy(() => import('./Components/Profile'));
-const EditProfile = lazy(() => import('./Screens/EditProfile'))
-const MakeSaleForm = lazy(() => import('./Screens/MakeSale'))
-const AllSales = lazy(() => import('./Screens/AllSales'))
-const Sales = lazy(() => import('./Components/Sales'))
-const Explore = lazy(() => import('./Components/StoryLine'))
-
-
-
-
+const EditProfile = lazy(() => import('./Screens/EditProfile'));
+const MakeSaleForm = lazy(() => import('./Screens/MakeSale'));
+const AllSales = lazy(() => import('./Screens/AllSales'));
+const Sales = lazy(() => import('./Components/Sales'));
+const Explore = lazy(() => import('./Components/StoryLine'));
 
 function App() {
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState(null)
-  const [userdetails, setUserDetails] = useState(null)
+  const [user, setUser] = useState(null);
+  const [userdetails, setUserDetails] = useState(null);
   const [clothes, setClothes] = useState([]);
-
 
   useEffect(() => {
     try {
-      const secretKey = "wabebee_x1_levick";
-      const encryptedUserID = localStorage.getItem("user_id");
-      setUserDetails(localStorage.getItem("username"))
+      const secretKey = 'wabebee_x1_levick';
+      const encryptedUserID = localStorage.getItem('user_id');
+      setUserDetails(localStorage.getItem('username'));
       if (encryptedUserID) {
-
         const bytes = CryptoJS.AES.decrypt(encryptedUserID, secretKey);
         const decryptedUserData = bytes.toString(CryptoJS.enc.Utf8);
 
@@ -50,24 +39,23 @@ function App() {
           const currentUser = JSON.parse(decryptedUserData);
           setUser(currentUser);
         } else {
-          console.error("Please log in.");
+          console.error('Please log in.');
         }
       } else {
-        console.error("Please log in.");
+        console.error('Please log in.');
       }
     } catch (error) {
-      console.error("Error decrypting user data:", error);
+      console.error('Error decrypting user data:', error);
     }
   }, []);
 
   useEffect(() => {
     // Fetch clothes data from the API
-    fetch("https://levick-6ab9bbf8750f.herokuapp.com/cloths")
+    fetch('https://levick-6ab9bbf8750f.herokuapp.com/cloths')
       .then((response) => response.json())
       .then((data) => setClothes(data))
-      .catch((error) => console.error("Error fetching clothes:", error));
+      .catch((error) => console.error('Error fetching clothes:', error));
   }, []);
-
 
   useEffect(() => {
     const session = localStorage.getItem('session_id');
@@ -78,56 +66,106 @@ function App() {
     }
   }, []);
 
-
-
   return (
     <div>
       <Helmet>
         <title>{userdetails ? `${userdetails} - Levick 23` : 'Levick 23 - Trendy and Affordable Clothing'}</title>
         <meta name="description" content="Welcome to Levick 23, your ultimate destination for trendy and affordable clothing! At Levick 23, we believe that fashion should be a reflection of individuality, style, and confidence. Our carefully curated collection features a diverse range of chic and contemporary apparel, designed to inspire and empower fashion-forward individuals like you." />
       </Helmet>
-      <Routes>
-        <Route path='/contact' element={<ContactPage />}></Route>
-        <Route path={ROUTES.home} element={<Suspense fallback={
-          <LoadingSpinner />
-        }><Home user={user} userdetails={userdetails} isLoggedIn={isLoggedIn} clothes={clothes} /></Suspense>} />
+      <CloudinaryContext cloudName="djmvocl1y">
+        <Routes>
+          <Route path="/contact" element={<ContactPage />} />
+          <Route
+            path={ROUTES.home}
+            element={(
+              <Suspense fallback={<LoadingSpinner />}>
+                <Home user={user} userdetails={userdetails} isLoggedIn={isLoggedIn} clothes={clothes} />
+              </Suspense>
+            )}
+          />
 
-        <Route path={ROUTES.clothComments} element={<Suspense fallback={
-          <LoadingSpinner />
-        }  > <CommentHandler user={user} /></Suspense>} />
-        <Route path={ROUTES.profile} element={<Suspense fallback={<div className="loading-spinner">
-          <div className="spinner"></div>
-        </div>}> < Profile user={user} userdetails={userdetails} /> </Suspense>} />
-        <Route path={ROUTES.editProfile} element={<Suspense fallback={<div className="loading-spinner">
-          <div className="spinner"></div>
-        </div>}> < EditProfile user={user} /> </Suspense>} />
-        <Route path={ROUTES.sales} element={<Suspense fallback={
-          <LoadingSpinner />
-        }><Sales clothes={clothes} user={user} /></Suspense>} />
-        <Route path={ROUTES.makeSale} element={<Suspense fallback={
-          <LoadingSpinner />
-        }><MakeSaleForm user={user} userdetails={userdetails} /></Suspense>} />
-        <Route path={ROUTES.allSales} element={<Suspense fallback={
-          <LoadingSpinner />
-        }><AllSales user={user} /></Suspense>} />
-        <Route path={ROUTES.explore} element={<Suspense fallback={
-          <LoadingSpinner />
-        }><Explore user={user} clothes={clothes} /></Suspense>} />
+          <Route
+            path={ROUTES.clothComments}
+            element={(
+              <Suspense fallback={<LoadingSpinner />}>
+                <CommentHandler user={user} />
+              </Suspense>
+            )}
+          />
+          <Route
+            path={ROUTES.profile}
+            element={(
+              <Suspense fallback={<div className="loading-spinner"><div className="spinner"></div></div>}>
+                <Profile user={user} userdetails={userdetails} />
+              </Suspense>
+            )}
+          />
+          <Route
+            path={ROUTES.editProfile}
+            element={(
+              <Suspense fallback={<div className="loading-spinner"><div className="spinner"></div></div>}>
+                <EditProfile user={user} />
+              </Suspense>
+            )}
+          />
+          <Route
+            path={ROUTES.sales}
+            element={(
+              <Suspense fallback={<LoadingSpinner />}>
+                <Sales clothes={clothes} user={user} />
+              </Suspense>
+            )}
+          />
+          <Route
+            path={ROUTES.makeSale}
+            element={(
+              <Suspense fallback={<LoadingSpinner />}>
+                <MakeSaleForm user={user} userdetails={userdetails} />
+              </Suspense>
+            )}
+          />
+          <Route
+            path={ROUTES.allSales}
+            element={(
+              <Suspense fallback={<LoadingSpinner />}>
+                <AllSales user={user} />
+              </Suspense>
+            )}
+          />
+          <Route
+            path={ROUTES.explore}
+            element={(
+              <Suspense fallback={<LoadingSpinner />}>
+                <Explore user={user} clothes={clothes} />
+              </Suspense>
+            )}
+          />
 
-        {/* Render the SignUpFo<Route path=''rm route only when the user is not logged in */}
-        {!isLoggedIn && (
-          <Route path={ROUTES.signUp} element={<Suspense fallback={
-            <LoadingSpinner />
-          }><SignUpForm /></Suspense>} />
-        )}
-        {/* Render the SignInForm route only when the user is not logged in */}
-        {!isLoggedIn && (
-          <Route path={ROUTES.signIn} element={<Suspense fallback={
-            <LoadingSpinner />
-          }><SignInForm setUser={setUser} /></Suspense>} />
-        )}
+          {/* Render the SignUpForm route only when the user is not logged in */}
+          {!isLoggedIn && (
+            <Route
+              path={ROUTES.signUp}
+              element={(
+                <Suspense fallback={<LoadingSpinner />}>
+                  <SignUpForm />
+                </Suspense>
+              )}
+            />
+          )}
+          {/* Render the SignInForm route only when the user is not logged in */}
+          {!isLoggedIn && (
+            <Route
+              path={ROUTES.signIn}
+              element={(
+                <Suspense fallback={<LoadingSpinner />}>
+                  <SignInForm setUser={setUser} />
+                </Suspense>
+              )}
+            />
+          )}
 
-      </Routes>
+        </Routes>
+      </CloudinaryContext>
     </div>
   );
 }
