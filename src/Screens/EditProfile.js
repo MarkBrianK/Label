9de10095d97ea9ff21d6615840c2ad7aea3d2ada
Kariuki Karanja <kariukimarkbrian@gmail.com>
2023-12-03 13:55:24 +0300

    :art:

--- src/Screens/EditProfile.js
+++ src/Screens/EditProfile.js
@@ -15,16 +15,13 @@ function EditProfile({ user }) {
 
   const [selectedFile, setSelectedFile] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
-  const [isUpdated, setIsUpdated] = useState(false);
   const editorRef = useRef();
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchUserData = async (user) => {
+    const fetchUserData = async () => {
       try {
-        const response = await axios.get(
-          `https://levick-6ab9bbf8750f.herokuapp.com/users/${user}`
-        );
+        const response = await axios.get(`http://127.0.0.1:3000/users/${user}`);
         const { username, mobile_number, county, profile_picture } = response.data;
         setFormData({
           username,
@@ -38,7 +35,7 @@ function EditProfile({ user }) {
     };
 
     if (user) {
-      fetchUserData(user);
+      fetchUserData();
     }
   }, [user]);
 
@@ -71,33 +68,30 @@ function EditProfile({ user }) {
     }
 
     const updatedFormData = new FormData();
-    updatedFormData.append("username", formData.username);
-    updatedFormData.append("mobile_number", formData.mobileNumber);
-    updatedFormData.append("county", formData.county);
+    updatedFormData.append("user[username]", formData.username);
+    updatedFormData.append("user[mobile_number]", formData.mobileNumber);
+    updatedFormData.append("user[county]", formData.county);
 
     if (selectedFile) {
-      updatedFormData.append("profile_picture", selectedFile);
+      updatedFormData.append("user[profile_picture]", selectedFile);
     }
 
     try {
-      await axios.patch(
-        `https://levick-6ab9bbf8750f.herokuapp.com/users/${user}`,
-        updatedFormData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
+      await axios.patch(`https://levick-6ab9bbf8750f.herokuapp.com/users/${user}`, updatedFormData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
 
-      setIsUpdated(true);
-      setIsEditing(false);
-      // Redirect to the user's profile page
+      // Redirect to the user's profile page on successful update
       navigate(`/profile/${user}`);
     } catch (error) {
       console.error("Error updating profile:", error);
     }
   };
+
+
+
 
   return (
     <div className={styles.centeredContainer}>
@@ -105,85 +99,88 @@ function EditProfile({ user }) {
         Edit Profile
       </label>
       <div className={styles.formContainer}>
-        <div className={styles.avatarContainer}>
         {isEditing && (
-  <div className={styles.avatarContainer}>
-    {formData.profilePicture && (
-      <AvatarEditor
-        ref={editorRef}
-        image={formData.profilePicture}
-        width={200}
-        height={200}
-        border={50}
-        borderRadius={100}
-        scale={1.2}
-      />
-    )}
-    <button className={styles.saveButton} onClick={handleSaveImage}>
-      Save
-    </button>
-  </div>
-)}
-          {!isEditing && (
-            <img
-              src={formData.profilePicture || "https://static-00.iconduck.com/assets.00/profile-circle-icon-512x512-zxne30hp.png"}
-              alt="Default Profile"
-              className={styles.defaultProfilePicture}
-            />
-          )}
-          <input
-            type="file"
-            id="profilePicture"
-            accept="image/*"
-            className={styles.profilePictureInput}
-            onChange={handleFileChange}
-          />
-          <div class="mb-3">
-            <label for="formFile" className={styles.uploadName}>Upload Profile Picture:</label>
-            <input class="form-control" type="file" id="formFile" onChange={handleFileChange} />
+          <div className={styles.avatarContainer}>
+            {formData.profilePicture && (
+              <AvatarEditor
+                ref={editorRef}
+                image={formData.profilePicture}
+                width={200}
+                height={200}
+                border={50}
+                borderRadius={100}
+                scale={1.2}
+              />
+            )}
+            <button className={styles.saveButton} onClick={handleSaveImage}>
+              Save
+            </button>
           </div>
-        </div>
-        <div className={styles.formElement}>
-          <label htmlFor="username" className={styles.formLabel}>
-            Username:
-          </label>
-          <input
-            type="text"
-            id="username"
-            className={styles.formInput}
-            value={formData.username}
-            onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+        )}
+        {!isEditing && (
+          <img
+            src={
+              formData.profilePicture ||
+              "https://static-00.iconduck.com/assets.00/profile-circle-icon-512x512-zxne30hp.png"
+            }
+            alt="Default Profile"
+            className={styles.defaultProfilePicture}
           />
-        </div>
-        <div className={styles.formElement}>
-          <label htmlFor="mobileNumber" className={styles.formLabel}>
-            Mobile Number:
+        )}
+        <input
+          type="file"
+          id="profilePicture"
+          accept="image/*"
+          className={styles.profilePictureInput}
+          onChange={handleFileChange}
+        />
+        <div class="mb-3">
+          <label for="formFile" className={styles.uploadName}>
+            Upload Profile Picture:
           </label>
-          <input
-            type="text"
-            id="mobileNumber"
-            className={styles.formInput}
-            value={formData.mobileNumber}
-            onChange={(e) => setFormData({ ...formData, mobileNumber: e.target.value })}
-          />
+          <input class="form-control" type="file" id="formFile" onChange={handleFileChange} />
         </div>
-        <div className={styles.formElement}>
-          <label htmlFor="county" className={styles.formLabel}>
-            County:
-          </label>
-          <br/>
-          <input
-            type="text"
-            id="county"
-            className={styles.formInput}
-            value={formData.county}
-            onChange={(e) => setFormData({ ...formData, county: e.target.value })}
-          />
-        </div>
-        <button className={styles.updateButton} onClick={handleUpdateProfile}>
-          Update Profile
-        </button>
       </div>
+      <div className={styles.formElement}>
+        <label htmlFor="username" className={styles.formLabel}>
+          Username:
+        </label>
+        <input
+          type="text"
+          id="username"
+          className={styles.formInput}
+          value={formData.username}
+          onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+        />
+      </div>
+      <div className={styles.formElement}>
+        <label htmlFor="mobileNumber" className={styles.formLabel}>
+          Mobile Number:
+        </label>
+        <input
+          type="text"
+          id="mobileNumber"
+          className={styles.formInput}
+          value={formData.mobileNumber}
+          onChange={(e) => setFormData({ ...formData, mobileNumber: e.target.value })}
+        />
+      </div>
+      <div className={styles.formElement}>
+        <label htmlFor="county" className={styles.formLabel}>
+          County:
+        </label>
+        <br />
+        <input
+          type="text"
+          id="county"
+          className={styles.formInput}
+          value={formData.county}
+          onChange={(e) => setFormData({ ...formData, county: e.target.value })}
+        />
+      </div>
+      <button className={styles.updateButton} onClick={handleUpdateProfile}>
+        Update Profile
+      </button>
       <Header user={user} />
     </div>
   );
